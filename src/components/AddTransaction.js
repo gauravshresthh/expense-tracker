76ccf362fe60c429d89034bcf449450f56738356
Gaurav Shrestha <gauravshresthh@gmail.com>
2020-08-10 07:00:19 +0545

    add transaction functionality added and completed the app

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
   const [amount, setAmount] = useState('');
 
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
   return (
     <>
       <h3>Add new transaction</h3>
-      <form id="form">
+      <form id="form" onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
